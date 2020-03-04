558535cc447ca9c9a6b4e0e366a99cb7ebe17377
Jordi <ofineo@gmail.com>
2020-03-04 18:02:09 +0000

    implemented search for artist

--- app.py
+++ app.py
@@ -166,7 +166,10 @@ def search_venues():
         "num_upcoming_shows": 0,
     })
     
-  return render_template('pages/search_venues.html', results=response, number=len(search_result),search_by=search_by)
+  return render_template('pages/search_venues.html', 
+  results=response, 
+  number=len(search_result),
+  search_by=search_by)
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
@@ -312,15 +315,29 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  search_by = request.form.get('search_term')
+  search_result = Artist.query.filter(Artist.name.ilike(f'%{search_by}%')).all()
+  response=[]
+
+  for result in search_result:
+      response.append({
+        "id": result.id,
+        "name": result.name,
+        "num_upcoming_shows": 0,
+    })
+
+  # response={
+  #   "count": 1,
+  #   "data": [{
+  #     "id": 4,
+  #     "name": "Guns N Petals",
+  #     "num_upcoming_shows": 0,
+  #   }]
+  # }
+  return render_template('pages/search_artists.html', 
+  results=response, 
+  number= len(search_result),
+  search_term=search_by)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
